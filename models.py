beda28cd4bb780c98386996043c27a70e03142a5
Gabriel Maldonado <45413354+gabola24@users.noreply.github.com>
2019-06-11 16:20:39 -0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+def get_lineal_model(num_class):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(32,32,3)))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+    return model
+
+def get_letnet_model(num_class):
+    model = tf.keras.Sequential()
+    model.add(
+        tf.keras.layers.Conv2D(
+            filters=6, kernel_size=(5,5), activation='tanh',
+            padding='same', input_shape=(32, 32, 3)
+        )
+    )
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Conv2D(16, (5,5), activation='tanh'))
+    model.add(tf.keras.layers.AveragePooling2D((2,2)))
+    model.add(tf.keras.layers.Conv2D(120, (5,5), activation='tanh'))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(84, activation='tanh'))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+return model
