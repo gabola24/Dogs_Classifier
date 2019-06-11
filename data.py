6dc3f27a6a29e06de7976bcfe055b4a3b9bc93e5
Gabriel Maldonado <45413354+gabola24@users.noreply.github.com>
2019-06-11 16:16:16 -0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,172 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import xml.etree.ElementTree as ET
+from PIL import Image
+import os
+import uuid
+import json
+import pandas as pd
+
+def read_content(xml_file: str):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    list_with_all_boxes = []
+    list_with_all_names = []
+
+    for boxes in root.iter('object'):
+
+        file_name = root.find('filename').text
+        file_path = root.find('path').text
+        name = boxes.find('name').text
+
+        ymin, xmin, ymax, xmax = None, None, None, None
+
+        for box in boxes.findall("bndbox"):
+            ymin = int(box.find("ymin").text)
+            xmin = int(box.find("xmin").text)
+            ymax = int(box.find("ymax").text)
+            xmax = int(box.find("xmax").text)
+
+        list_with_single_boxes = [xmin, ymin, xmax, ymax]
+        list_with_all_boxes.append(list_with_single_boxes)
+        list_with_all_names.append(name)
+
+    return file_path,file_name, list_with_all_boxes,list_with_all_names
+
+path='/home/gabriel24/MeetupDL/imagenes/'
+path_to_images='/home/gabriel24/MeetupDL/imagenes/origin'
+dic='labels_map.json'
+
+def save_xmls(directory):
+    w=[]
+    for file in os.listdir(directory):
+        if file.endswith(".xml"):
+            w.append(read_content(directory+file))
+    return(w)
+
+
+def crops_and_metadata(directory,jason_dic):
+    with open(jason_dic) as f:
+        dic = json.loads(f.read())
+    os.mkdir('recortes')
+    names,splits,labl=[],[],[]
+    for i in save_xmls(directory):
+        p=Image.open(path_to_images+i[1])
+        p = np.asarray(p)
+        
+        for j in range(len(i[3])):
+            a=p[i[2][j][1]:i[2][j][3],i[2][j][0]:i[2][j][2],0:3]
+            a=Image.fromarray(a)
+            name=uuid.uuid4().hex
+            names.append(name+'.png')
+            a.save('recortes/'+name+'.png')
+            split = np.random.rand(1)[0]
+            if split<0.7:
+                spl='train'
+            else:
+                spl='test'
+            splits.append(spl)
+            labl.append(i[3][j])
+    df=pd.DataFrame()
+    df['names']=names
+    df['split']=splits
+    df['label']=labl
+    df=df.replace(dic)
+    df.to_csv('metadata.csv')
+    return df
+
+        
+def build_sources_from_metadata(metadata, data_dir, mode='train', exclude_labels=None):
+    
+    if exclude_labels is None:
+        exclude_labels = set()
+    if isinstance(exclude_labels, (list, tuple)):
+        exclude_labels = set(exclude_labels)
+
+    df = metadata.copy()
+    df = df[df['split'] == mode]
+    df['filepath'] = df['names'].apply(lambda x: os.path.join(data_dir, x))
+    include_mask = df['label'].apply(lambda x: x not in exclude_labels)
+    df = df[include_mask]
+
+    sources = list(zip(df['filepath'], df['label']))
+    return sources
+      
+def imshow_batch_of_three(batch, show_label=True):
+    label_batch = batch[1].numpy()
+    image_batch = batch[0].numpy()
+    fig, axarr = plt.subplots(1, 3, figsize=(15, 5), sharey=True)
+    for i in range(3):
+        img = image_batch[i, ...]
+        axarr[i].imshow(img)
+        if show_label:
+            axarr[i].set(xlabel='label = {}'.format(label_batch[i]))
+
+def preprocess_image(image):
+    image = tf.image.resize(image, size=(32, 32))
+    image = image / 255.0
+    return image
+
+def augment_image(image):
+    return image
+
+def make_dataset(sources, training=False, batch_size=1, num_epochs=1, num_parallel_calls=1, shuffle_buffer_size=None):
+    """
+    Returns an operation to iterate over the dataset specified in sources
+
+    Args:
+        sources (list): A list of (filepath, label_id) pairs.
+        training (bool): whether to apply certain processing steps
+            defined only in training mode (e.g. shuffle).
+        batch_size (int): number of elements the resulting tensor
+            should have.
+        num_epochs (int): Number of epochs to repeat the dataset.
+        num_parallel_calls (int): Number of parallel calls to use in
+            map operations.
+        shuffle_buffer_size (int): Number of elements from this dataset
+            from which the new dataset will sample.
+
+    Returns:
+        A tf.data.Dataset object. It will return a tuple images of shape
+        [N, H, W, CH] and labels shape [N, 1].
+    """
+    def load(row):
+        filepath = row['image']
+        img = tf.io.read_file(filepath)
+        img = tf.io.decode_jpeg(img)
+        return img, row['label']
+
+    if shuffle_buffer_size is None:
+        shuffle_buffer_size = batch_size*4
+
+    images, labels = zip(*sources)
+    
+    ds = tf.data.Dataset.from_tensor_slices({
+        'image': list(images), 'label': list(labels)}) 
+ #line from the link. As with most code, if you remove an arbitrary line, expectin
+    if training:
+        ds = ds.shuffle(shuffle_buffer_size)
+    
+    ds = ds.map(load, num_parallel_calls=num_parallel_calls)
+    ds = ds.map(lambda x,y: (preprocess_image(x), y))
+    
+    if training:
+        ds = ds.map(lambda x,y: (augment_image(x), y))
+        
+    ds = ds.repeat(count=num_epochs)
+    ds = ds.batch(batch_size=batch_size)
+    ds = ds.prefetch(1)
+
+    return ds
+       
+        
+        
+        
+        
+        
+        
+        
+    
+    
+
