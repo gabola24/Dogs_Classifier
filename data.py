7e26fda3b2131f483867b5e8a7a21a654c16d794
Gabriel Maldonado <gabo-2400@hotmail.com>
2019-07-09 16:35:29 -0500

    correccion

--- data.py
+++ data.py
@@ -112,7 +112,57 @@ def preprocess_image(image):
 def augment_image(image):
     return image
 
-def make_dataset(sources, training=False, batch_size=1, num_epochs=1, num_parallel_calls=1, shuffle_buffer_size=None):
+def make_dataset(sources, training=False, batch_size=1,
+    num_epochs=1, num_parallel_calls=1, shuffle_buffer_size=None, pixels = 32, target = 1):
+    """
+    Returns an operation to iterate over the dataset specified in sources
+    Args:
+        sources (list): A list of (filepath, label_id) pairs.
+        training (bool): whether to apply certain processing steps
+            defined only in training mode (e.g. shuffle).
+        batch_size (int): number of elements the resulting tensor
+            should have.
+        num_epochs (int): Number of epochs to repeat the dataset.
+        num_parallel_calls (int): Number of parallel calls to use in
+            map operations.
+        shuffle_buffer_size (int): Number of elements from this dataset
+            from which the new dataset will sample.
+        pixels (int): Size of the image after resize 
+    Returns:
+        A tf.data.Dataset object. It will return a tuple images of shape
+        [N, H, W, CH] and labels shape [N, 1].
+    """
+    def load(row):
+        filepath = row['image']
+        img = tf.io.read_file(filepath)
+        img = tf.io.decode_jpeg(img)
+        return img, row['label']
+
+    if shuffle_buffer_size is None:
+        shuffle_buffer_size = batch_size*4
+
+    images, labels = zip(*sources)
+    
+    ds = tf.data.Dataset.from_tensor_slices({
+        'image': list(images), 'label': list(labels)}) 
+
+    if training:
+        ds = ds.shuffle(shuffle_buffer_size)
+    
+    ds = ds.map(load, num_parallel_calls=num_parallel_calls)
+    ds = ds.map(lambda x,y: (preprocess_image(x, pixels), y))
+    
+    if training:
+        ds = ds.map(lambda x,y: (augment_image(x), y))
+        
+    ds = ds.map(lambda x, y: (x, tuple([y]*target) if target > 1 else y))
+    ds = ds.repeat(count=num_epochs)
+    ds = ds.batch(batch_size=batch_size)
+    ds = ds.prefetch(1)
+
+    return ds
+
+def make_dataset2(sources, training=False, batch_size=1, num_epochs=1, num_parallel_calls=1, shuffle_buffer_size=None):
     import tensorflow as tf
     """
     Returns an operation to iterate over the dataset specified in sources
