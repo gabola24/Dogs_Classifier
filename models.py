bf656ec2344804f57ad68b02d30eebfbf3e6db66
gabola24 <gabo-2400@hotmail.com>
2019-07-30 19:11:29 -0500

    resnet

--- models.py
+++ models.py
@@ -156,27 +156,22 @@ def get_vgg16_model(num_class):
     return model
 
 def get_google_net_model(num_class):
-
     input_layer = tf.keras.layers.Input(shape=(224, 224, 3))
-
     x = tf.keras.layers.Conv2D(64, (7, 7), padding='same', strides=(2, 2), activation='relu', name='conv_1_7x7/2')(input_layer)
     x = tf.keras.layers.MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_1_3x3/2')(x)
     x = tf.keras.layers.Conv2D(64, (1, 1), padding='same', strides=(1, 1), activation='relu', name='conv_2a_3x3/1')(x)
     x = tf.keras.layers.Conv2D(192, (3, 3), padding='same', strides=(1, 1), activation='relu', name='conv_2b_3x3/1')(x)
     x = tf.keras.layers.MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_2_3x3/2')(x)
-
     x = inception_module(x, 64, 96, 128, 16, 32, 32)
     x = inception_module(x, 128, 128, 192, 32, 96, 64)
     x = tf.keras.layers.MaxPool2D((3, 3), padding='same', strides=(2, 2), name='max_pool_3_3x3/2')(x)
     x = inception_module(x, 192, 96, 208, 16, 48, 64)
-
     x1 = tf.keras.layers.AveragePooling2D((5, 5), strides=3)(x)
     x1 = tf.keras.layers.Conv2D(128, (1, 1), padding='same', activation='relu')(x1)
     x1 = tf.keras.layers.Flatten()(x1)
     x1 = tf.keras.layers.Dense(1024, activation='relu')(x1)
     x1 = tf.keras.layers.Dropout(0.7)(x1)
     x1 = tf.keras.layers.Dense(num_class, activation='softmax', name='auxilliary_output_1')(x1)
-
     x = inception_module(x, 160, 112, 224, 24, 64, 64)
     x = inception_module(x, 128, 128, 256, 24, 64, 64)
     x = inception_module(x, 112, 144, 288, 32, 64, 64)
@@ -320,3 +315,124 @@ def build_standard_cnn(
     model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
     
     return model
+def identity_block(X, f, filters, stage, block):
+    
+    # Defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value
+    X_shortcut = X
+    
+    # First component of main path
+    X = tf.keras.layers.Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = tf.keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = tf.keras.layers.Activation('relu')(X)
+    
+    # Second component of main path
+    X = tf.keras.layers.Conv2D(filters = F2, kernel_size = (f, f), strides = (1, 1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = tf.keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = tf.keras.layers.Activation('relu')(X)
+
+    # Third component of main path 
+    X = tf.keras.layers.Conv2D(filters = F3, kernel_size = (1, 1), strides = (1, 1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = tf.keras.layers.BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation
+    X = tf.keras.layers.Add()([X, X_shortcut])
+    X = tf.keras.layers.Activation('relu')(X)
+    
+    return X
+def convolutional_block(X, f, filters, stage, block, s=2):
+
+    # Defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
+
+    # Save the input value
+    X_shortcut = X
+
+    ##### MAIN PATH #####
+    # First component of main path 
+    X = tf.keras.layers.Conv2D(filters=F1, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '2a', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = tf.keras.layers.BatchNormalization(axis=3, name=bn_name_base + '2a')(X)
+    X = tf.keras.layers.Activation('relu')(X)
+
+    # Second component of main path
+    X = tf.keras.layers.Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = tf.keras.layers.BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+    X = tf.keras.layers.Activation('relu')(X)
+
+    # Third component of main path
+    X = tf.keras.layers.Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+    X = tf.keras.layers.BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+    ##### SHORTCUT PATH #### 
+    X_shortcut = tf.keras.layers.Conv2D(filters=F3, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '1', kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = tf.keras.layers.BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation
+    X = tf.keras.layers.Add()([X, X_shortcut])
+    X = tf.keras.layers.Activation('relu')(X)
+
+    return X
+
+def ResNet50(input_shape = (224, 224, 3), classes = 6):
+    
+    # Define the input as a tensor with shape input_shape
+    X_input = tf.keras.layers.Input(input_shape)
+
+    
+    # Zero-Padding
+    X = tf.keras.layers.ZeroPadding2D((3, 3))(X_input)
+    
+    # Stage 1
+    X = tf.keras.layers.Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = tf.keras.layers.BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+    X = tf.keras.layers.Activation('relu')(X)
+    X = tf.keras.layers.MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage 2
+    X = convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    # Stage 3
+    X = convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+    # Stage 4
+    X = convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    # Stage 5
+    X = convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    # AVGPOOL
+    X = tf.keras.layers.AveragePooling2D(pool_size=(2,2), padding='same')(X)
+
+    # Output layer
+    X = tf.keras.layers.Flatten()(X)
+    X = tf.keras.layers.Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+    
+    
+    # Create model
+    model = tf.keras.Model(inputs = X_input, outputs = X, name='ResNet50')
+
+    return model
+
+
